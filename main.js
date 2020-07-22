27e701d14d691232420c63d2ef5caa2e019d1847
Fabrizio <fabriziospoto1@gmail.com>
2020-07-22 18:58:05 +0200

    Responsive Menu 2
    Responsive Menu 2

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var respMenu = false;
+// var respMenu = false;
+//
+// $('i').click(function() {
+//     if (respMenu == true) {
+//         $('.hamburger-menu').show();
+//         respMenu = false;
+//     } else {
+//         $('.hamburger-menu').hide();
+//         respMenu = true;
+//     }
+// });
 
-$('i').click(function() {
-    if (respMenu == true) {
-        $('.hamburger-menu').show();
-        respMenu = false;
-    } else {
-        $('.hamburger-menu').hide();
-        respMenu = true;
-    }
+$('.header-right>a').click(function(){
+    $('.hamburger-menu').addClass('active');
+});
+
+$('.close').click(function(){
+    $('.hamburger-menu').removeClass('active');
 });
